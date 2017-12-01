dc001d3af8a772002e8c90385f6cb312380ec809
idearobin2017 <idearobin@gmail.com>
2017-12-01 21:37:36 +0100

    Lift the state up and pass down a prop between Board component and Square component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state ={
-			value: null,
-		};
-	}
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        { this.state.value }
+      <button className="square" onClick={() => this.props.onClick()}>
+        { this.props.value }
       </button>
     );
   }
@@ -29,8 +22,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value = {this.state.squares[i]}
+				onClick = {() => this.handleClick(i)}
 			/>
 		);
   }
